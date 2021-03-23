69368d272dbdc78750a931dcdb811ea020c9b10e
周靖超 <jingchao.zhou@lilyenglish.com>
2021-03-23 18:09:22 +0800

    code

--- src/views/codeBoard/components/answer/answer.vue
+++ src/views/codeBoard/components/answer/answer.vue
@@ -47,7 +47,7 @@
         data(){
             return{
                 clientHeight: 0,
-                lang: 'javascript',
+                lang: 'python',
                 langList: [
                     {
                         label: 'Javascript',
@@ -94,8 +94,10 @@
                     mode: this.lang,//选择对应代码编辑器的语言，我这边选的是数据库，根据个人情况自行设置即可
                     indentWithTabs: true,
                     smartIndent: true,
+                    styleActiveLine: true,
                     lineNumbers: true,
                     matchBrackets: true,
+                    fixedGutter: false,
                     height: `${this.clientHeight}px`,
                     //theme: theme,
                     // autofocus: true,
@@ -110,6 +112,28 @@
                 editor.on('cursorActivity', function () {
                     editor.showHint()
                 });
+                editor.setSize('auto',`${this.clientHeight}px`);
+                editor.setValue( `
+//  代码示例：
+class counting_sort(object):
+  def _counting_sort(self, alist, k):
+    alist3 = [0 for i in range(k)]
+    alist2 = [0 for i in range(len(alist))]
+    for j in alist:
+      alist3[j] += 1
+    for i in range(1, k):
+      alist3[i] = alist3[i-1] + alist3[i]
+    for l in alist[::-1]:
+      alist2[alist3[l]-1] = l
+      alist3[l] -= 1
+    return alist2
+
+  def __call__(self, sort_list, k=None):
+    if k is None:
+      import heapq
+      k = heapq.nlargest(1, sort_list)[0] + 1
+    return self._counting_sort(sort_list, k)
+                `)
             },
         }
     }
